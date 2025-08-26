139a079f6f7ce34562611e93e100556e9a929a74
89rajkumaryadav <89rajkumaryadav@gmail.com>
2025-08-26 18:43:55 +0530

    Saving data using hibernate

--- HibProj/src/main/java/org/example/Main.java
+++ HibProj/src/main/java/org/example/Main.java
@@ -10,22 +10,8 @@ import org.hibernate.cfg.Configuration;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setsName("Raj");
-        student.setRollNo(104);
-        student.setsAge(35);
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(Student.class);
-        cfg.configure();
-        SessionFactory sf = cfg.buildSessionFactory();
-        Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(student);
-        transaction.commit();
-        session.close();
-        sf.close();
-        System.out.println(student);
+      //  SavingDataHibernate savingDataHibernate = new SavingDataHibernate();
 
 
     }
